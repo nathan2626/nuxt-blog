0bae6920b0b0d5b1489bf9b27918aa3df55b54e2
nathan2626 <nathanjourno@yahoo.fr>
2021-06-07 12:57:31 +0200

    Add errors meme

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="container d-flex justify-content-center align-items-center">
-    <div class="">
+    <div v-if="error.statusCode === 404" class="">
       <b-img fluid src="https://httpstatusdogs.com/img/404.jpg" alt=""></b-img>
+    </div>
+    <div v-else-if="error.statusCode === 400" class="">
+      <b-img fluid src="https://httpstatusdogs.com/img/400.jpg" alt=""></b-img>
+    </div>
+    <div v-else-if="error.statusCode === 401" class="">
+      <b-img fluid src="https://httpstatusdogs.com/img/401.jpg" alt=""></b-img>
+    </div>
+    <div v-else-if="error.statusCode === 402" class="">
+      <b-img fluid src="https://httpstatusdogs.com/img/402.jpg" alt=""></b-img>
+    </div>
+    <div v-else-if="error.statusCode === 405" class="">
+      <b-img fluid src="https://httpstatusdogs.com/img/405.jpg" alt=""></b-img>
+    </div>
+    <div v-else-if="error.statusCode === 403" class="">
+      <b-img fluid src="https://httpstatusdogs.com/img/403.jpg" alt=""></b-img>
+    </div>
+    <div v-else class="">
+      <b-img fluid src="https://support.apple.com/library/content/dam/edam/applecare/images/fr_FR/macos/Mojave/macos-mojave-restart-problem-ignore-report-alert.jpg" alt=""></b-img>
     </div>
   </div>
 </template>
